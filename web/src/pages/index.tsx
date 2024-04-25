6bee77316204e270c882b2380d3abdcf44dca900
Sergey Sidorov <sergit2292@gmail.com>
2024-04-25 12:17:50 +0300

    some fix

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,74 +1,86 @@
 import Head from "next/head";
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import Table from "react-bootstrap/Table";
-import {Alert, Container} from "react-bootstrap";
-import {GetServerSideProps, GetServerSidePropsContext} from "next";
+import { Alert, Container, Pagination } from "react-bootstrap";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 type TUserItem = {
-  id: number
-  firstname: string
-  lastname: string
-  email: string
-  phone: string
-  updatedAt: string
-}
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  updatedAt: string;
+};
 
 type TGetServerSideProps = {
-  statusCode: number
-  users: TUserItem[]
-}
-
+  statusCode: number;
+  users: TUserItem[];
+};
 
 export const getServerSideProps = (async (ctx: GetServerSidePropsContext): Promise<{ props: TGetServerSideProps }> => {
   try {
-    const res = await fetch("http://localhost:3000/users", {method: 'GET'})
+    const res = await fetch("http://localhost:3000/users", { method: "GET" });
     if (!res.ok) {
-      return {props: {statusCode: res.status, users: []}}
+      return { props: { statusCode: res.status, users: [] } };
     }
 
     return {
-      props: {statusCode: 200, users: await res.json()}
-    }
+      props: { statusCode: 200, users: await res.json() },
+    };
   } catch (e) {
-    return {props: {statusCode: 500, users: []}}
+    return { props: { statusCode: 500, users: [] } };
   }
-}) satisfies GetServerSideProps<TGetServerSideProps>
+}) satisfies GetServerSideProps<TGetServerSideProps>;
 
-
-export default function Home({statusCode, users}: TGetServerSideProps) {
+export default function Home({ statusCode, users }: TGetServerSideProps) {
   if (statusCode !== 200) {
-    return <Alert variant={'danger'}>Ошибка {statusCode} при загрузке данных</Alert>
+    return <Alert variant={"danger"}>Ошибка {statusCode} при загрузке данных</Alert>;
   }
 
+  let active = 1;
+  let items = [];
+  for (let number = 1; number <= 10; number++) {
+    items.push(
+      <Pagination.Item key={number} active={number === active}>
+        {number}
+      </Pagination.Item>
+    );
+  }
+
+  const paginationBasic = (
+    <div>
+      <Pagination>{items}</Pagination>
+    </div>
+  );
   return (
     <>
       <Head>
         <title>Тестовое задание</title>
-        <meta name="description" content="Тестовое задание"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link rel="icon" href="/favicon.ico"/>
+        <meta name="description" content="Тестовое задание" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={inter.className}>
         <Container>
-          <h1 className={'mb-5'}>Пользователи</h1>
+          <h1 className={"mb-5"}>Пользователи</h1>
 
           <Table striped bordered hover>
             <thead>
-            <tr>
-              <th>ID</th>
-              <th>Имя</th>
-              <th>Фамилия</th>
-              <th>Телефон</th>
-              <th>Email</th>
-              <th>Дата обновления</th>
-            </tr>
+              <tr>
+                <th>ID</th>
+                <th>Имя</th>
+                <th>Фамилия</th>
+                <th>Телефон</th>
+                <th>Email</th>
+                <th>Дата обновления</th>
+              </tr>
             </thead>
             <tbody>
-            {
-              users.map((user) => (
+              {users.map((user) => (
                 <tr key={user.id}>
                   <td>{user.id}</td>
                   <td>{user.firstname}</td>
@@ -77,13 +89,11 @@ export default function Home({statusCode, users}: TGetServerSideProps) {
                   <td>{user.email}</td>
                   <td>{user.updatedAt}</td>
                 </tr>
-              ))
-            }
+              ))}
             </tbody>
           </Table>
-
+          {paginationBasic}
           {/*TODO add pagination*/}
-
         </Container>
       </main>
     </>
